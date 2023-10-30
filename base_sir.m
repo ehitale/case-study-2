a814d7bfbfcb37196ed0909366150e2533710b05
ehitale <elatihe@gmail.com>
2023-10-30 17:09:15 -0500

    added 'COVID_STL.MAT' and began graphing cases/death data.

--- base_sir.m
+++ base_sir.m
@@ -1,3 +1,18 @@
+% todo: match up times and convert cases and deaths to percentages.
+
+load("COVID_STL.mat")
+
+% The following creates a range of dates for the Delta and Omicron
+% variants.
+delta_range = isbetween(dates, "2021-6-30", "2021-10-26");
+omicron_range = isbetween(dates, "2021-10-27", "2022-3-22");
+
+% The following are case and death data indexed using the ranges above. 
+cases_delta = cases_STL(delta_range);
+cases_omicron = cases_STL(omicron_range);
+deaths_delta = deaths_STL(delta_range);
+deaths_omicron = deaths_STL(omicron_range); 
+
 % The following matrix implements the SIR dynamics example from Chapter 9.3
 % of the textbook.
 A = [0.95 0.04 0 0; 0.05 0.85 0 0; 0 0.1 1 0; 0 0.01 0 1];
@@ -16,6 +31,20 @@ Y = lsim(sys_sir_base,zeros(1000,1),linspace(0,999,1000),x0);
 
 % plot the output trajectory
 plot(Y);
-legend('S','I','R','D');
+% legend('S','I','R','D');
 xlabel('Time')
 ylabel('Percentage Population');
+hold on;
+
+% The following plots the actual death and case data for the two variants.
+% The time values need to match up!!!
+plot(deaths_delta)
+hold on;
+plot(cases_delta)
+hold on;
+
+plot(deaths_omicron)
+hold on;
+plot(cases_omicron)
+hold on;
+legend('S','I','R','D','deaths delta', 'cases delta', 'deaths omicron', 'cases omicron')

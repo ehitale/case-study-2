8645f25b804fa588f75ed477627631265198c32a
ehitale <elatihe@gmail.com>
2023-10-30 20:04:24 -0500

    plotted Y matrices and percent case/death data.

--- base_sir.m
+++ base_sir.m
@@ -11,7 +11,12 @@ omicron_range = isbetween(dates, "2021-10-27", "2022-3-22");
 cases_delta = cases_STL(delta_range);
 cases_omicron = cases_STL(omicron_range);
 deaths_delta = deaths_STL(delta_range);
-deaths_omicron = deaths_STL(omicron_range); 
+deaths_omicron = deaths_STL(omicron_range);
+
+percent_cases_delta = cases_delta/POP_STL;
+percent_cases_omicron = cases_omicron/POP_STL;
+percent_deaths_delta = deaths_delta/ POP_STL;
+percent_deaths_omicron = deaths_omicron/POP_STL;
 
 % The following matrix implements the SIR dynamics example from Chapter 9.3
 % of the textbook.
@@ -22,29 +27,42 @@ A = [0.95 0.04 0 0; 0.05 0.85 0 0; 0 0.1 1 0; 0 0.01 0 1];
 B = zeros(4,1);
 
 % initial conditions (i.e., values of S, I, R, D at t=0).
-x0 = [0.9 0.1 0 0];
+% todo: find the initial percentage of the population at the onset of the
+% delta variant? should I just do everything twice, just considering the
+% relevant time ranges each time?
+% x0 = [0.9 0.1 0 0];
+x0 = [1.0 0 0 0];
 
 % Here is a compact way to simulate a linear dynamical system.
 % Type 'help ss', 'help lsim', etc., to learn about how these functions work!!
 sys_sir_base = ss(A,B,eye(4),zeros(4,1),1);
 Y = lsim(sys_sir_base,zeros(1000,1),linspace(0,999,1000),x0);
+Y_delta= Y(delta_range);
+Y_omicron = Y(omicron_range);
 
-% plot the output trajectory
-plot(Y);
-% legend('S','I','R','D');
-xlabel('Time')
-ylabel('Percentage Population');
-hold on;
+% % plot the output trajectory
+% plot(Y);
+% % legend('S','I','R','D');
+% xlabel('Time')
+% ylabel('Percentage Population');
+% hold on;
 
 % The following plots the actual death and case data for the two variants.
 % The time values need to match up!!!
-plot(deaths_delta)
+figure;
+plot(Y_delta);
 hold on;
 plot(cases_delta)
 hold on;
+plot(deaths_delta)
+legend('Y delta', 'cases delta', 'deaths delta')
+hold off;
 
-plot(deaths_omicron)
+figure;
+plot(Y_omicron);
 hold on;
 plot(cases_omicron)
 hold on;
-legend('S','I','R','D','deaths delta', 'cases delta', 'deaths omicron', 'cases omicron')
+plot(deaths_omicron)
+legend('Y omicron', 'cases omicron', 'deaths omicron')
+hold off;

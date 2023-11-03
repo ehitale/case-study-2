664b7e768b891e143ae9af932dc8f23a5de58ec8
ehitale <elatihe@gmail.com>
2023-11-03 15:04:26 -0500

    no time.

--- input_sir.m
+++ input_sir.m
@@ -1,17 +1,19 @@
 % % The following matrix implements the SIR dynamics example from Chapter 9.3
 % % of the textbook.
 % A = [0.95 0.04 0 0; 0.05 0.85 0 0; 0 0.1 1 0; 0 0.01 0 1];
-A_m = [0.95 0.04 0 0; 0.05 0.85 0 0; 0 0.1 1 0; 0 0.01 0 1];
-A_g = [0.95 0.04 0 0; 0.05 0.85 0 0; 0 0.1 1 0; 0 0.01 0 1];
+A_sird1 = [0.95 0.04 0 0; 0.05 0.85 0 0; 0 0.1 1 0; 0 0.01 0 1];
+A_sird2 = [0.95 0.04 0 0; 0.05 0.85 0 0; 0 0.1 1 0; 0 0.01 0 1];
+travel1 = [0.2 .1 0 0; 0.2 .1 0 0; 0.2 .1 0 0; 0.2 .1 0 0];
+travel2 = [0.2 .1 0 0; 0.2 .1 0 0; 0.2 .1 0 0; 0.2 .1 0 0];
+
+A_mg = [cat(1, A_sird1 , travel1), cat(1, travel2, A_sird2)];
 
 % The following matrix is needed to use the lsim function to simulate the
 % system in question
-B_m = Y_g(1:end, :)';
-B_g = Y_m(1:end, :)';
+B_mg = zeros(8, 1);
 
 % initial conditions (i.e., values of S, I, R, D at t=0).
-x0_m = [0.9 0.1 0 0];
-x0_g = [0.8 0.2 0 0];
+x0_m = [0.9 0.1 0 0 0.8 0.2 0 0];
 
 % The two populations, Metropolis and Gotham City
 % These figures will be used to convert between relative percents.
@@ -30,19 +32,16 @@ gPop = 10000000; % 10 million
 
 % Here is a compact way to simulate a linear dynamical system.
 % Type 'help ss', 'help lsim', etc., to learn about how these functions work!!
-sys_sir_base_m = ss(A_m,B_m(:, 1:end),eye(4),zeros(4,1),1);
-Y_m = lsim(sys_sir_base_m,zeros(1000,1),linspace(0,999,1000),x0_m);
-
-sys_sir_base_g = ss(A_g,B_g(:, 1:end),eye(4),zeros(4,1),1);
-Y_g = lsim(sys_sir_base_g,zeros(1000,1),linspace(0,999,1000),x0_g);
-
-% plot the output trajectory
-plot(Y_m);
-legend('S','I','R','D');
-xlabel('Time')
-ylabel('Percentage Population');
-
-plot(Y_g);
-legend('S','I','R','D');
-xlabel('Time')
-ylabel('Percentage Population');
+% sys_sir_base_m = ss(A_mg,B,eye(4),zeros(4,1),1);
+% Y_m = lsim(sys_sir_base_m,zeros(1000,1),linspace(0,999,1000),x0_m);
+% 
+% % plot the output trajectory
+% plot(Y_m);
+% legend('S','I','R','D');
+% xlabel('Time')
+% ylabel('Percentage Population');
+% 
+% plot(Y_g);
+% legend('S','I','R','D');
+% xlabel('Time')
+% ylabel('Percentage Population');
